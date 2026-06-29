7e35a025499dd0c9707ce39ef2e4619da19004b1
Abdullah <abdullahyeteris1@gmail.com>
2026-06-30 01:50:38 +0300

    Epic UI-1A Design System Foundation completed

---
+++ src/components/__init__.py
@@ -0,0 +1,23 @@
+"""Shared UI components package exports."""
+
+from .badge import BADGE_VARIANTS, build_badge
+from .button import BUTTON_VARIANTS, build_button
+from .card import build_card
+from .dialog import build_dialog
+from .dropdown import build_dropdown
+from .search_box import build_search_box
+from .snackbar import build_snackbar
+from .text_field import build_text_field
+
+__all__ = [
+    "BADGE_VARIANTS",
+    "BUTTON_VARIANTS",
+    "build_badge",
+    "build_button",
+    "build_card",
+    "build_dialog",
+    "build_dropdown",
+    "build_search_box",
+    "build_snackbar",
+    "build_text_field",
+]
---
+++ src/theme/icons.py
@@ -0,0 +1,18 @@
+"""Icon tokens for the Idil design system."""
+
+
+class IconSizes:
+    """Standard icon size scale from docs/09-Design-System.md."""
+
+    XS = 16
+    SM = 20
+    MD = 24
+    LG = 32
+
+
+ICON_SCALE = {
+    "xs": IconSizes.XS,
+    "sm": IconSizes.SM,
+    "md": IconSizes.MD,
+    "lg": IconSizes.LG,
+}
