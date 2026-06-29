df49aabe9225eb6de7ee15d4ab48f9d6bd7c77ea
Abdullah <abdullahyeteris1@gmail.com>
2026-06-29 13:17:40 +0300

    Epic 3.2F ReportRepository completed

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -4,6 +4,7 @@ from .base_repository import BaseRepository
 from .course_repository import CourseRepository
 from .lesson_repository import LessonRepository
 from .measurement_repository import MeasurementRepository
+from .report_repository import ReportRepository
 from .schedule_repository import ScheduleRepository
 from .student_repository import StudentRepository
 
@@ -14,4 +15,5 @@ __all__ = [
 	"ScheduleRepository",
 	"LessonRepository",
 	"MeasurementRepository",
+	"ReportRepository",
 ]
